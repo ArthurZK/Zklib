23e71a63a6bdaf026bac666e520802f66fab7285
姬彦 <zackchow@192.168.2.1>
2019-04-28 10:08:17 +0800

    zack

---
+++ ZkTestProject/Classes/viewontrollers/MyViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyViewController.h
+//  Pods-ZkTestProject_Example
+//
+//  Created by zack chow on 2019/4/28.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyViewController : UIViewController
+- (NSString *)myTest;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZkTestProject/Classes/viewontrollers/MyViewController.m
@@ -0,0 +1,34 @@
+//
+//  MyViewController.m
+//  Pods-ZkTestProject_Example
+//
+//  Created by zack chow on 2019/4/28.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (NSString *)myTest{
+    return @"name";
+
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
